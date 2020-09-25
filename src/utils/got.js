4c2967c00d6701444adc5b208471792f50a295fb
yinshuaibin <35283085+yinshuaibin@users.noreply.github.com>
2020-09-25 11:06:01 +0800

    got适用于node.js服务器, 并不适用于浏览器, 故删除, 下次使用要看清到底能否使用

--- src/utils/got.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const got = require('got')
-var $got = {}
-$got.install = function (Vue, option) {
-  Object.defineProperty(Vue.prototype, '$got', { value: got })
-}
-export default $got
