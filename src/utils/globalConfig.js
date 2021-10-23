edac8211c0c70bab6feabf76707a53c8b1130794
yinshuaibin <35283085+yinshuaibin@users.noreply.github.com>
2021-10-23 12:22:14 +0800

    提交websocket测试

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -1,15 +1,17 @@
 // let声明变量只能在当前大括号内使用
 const env = process.env.NODE_ENV
 // const声明的是常亮,默认不可改变
+const domain = 'www.yinshuaibin.xyz'
 // 开发地址
 const devUrl = 'http://127.0.0.1:9998'
 // 生产环境地址
-const prodUrl = 'https://www.yinshuaibin.xyz/yinshuaibin'
+const prodUrl = 'https://' + domain + '/yinshuaibin'
 // 开发地址
-const devPicUrl = 'http://127.0.0.1:9998/image'
+const devPicUrl = devUrl + '/image'
 // 生产环境地址
-const prodPicUrl = 'https://www.yinshuaibin.xyz/pic'
+const prodPicUrl = 'https://' + domain + '/pic'
 // 备用地址
 const backUpUrl = 'http://127.0.0.1:10086'
 export const baseUrl = env === 'development' ? devUrl : env === 'production' ? prodUrl : backUpUrl
 export const basePicUrl = env === 'development' ? devPicUrl : env === 'production' ? prodPicUrl : backUpUrl
+export const webSocketUrl = env === 'development' ? devUrl.replace('http', 'ws') : env === 'production' ? 'wss://' + domain : backUpUrl
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,37 @@
+<style scoped>
+  .websocket {
+    background:#eee;
+    padding: 20px;
+    height: 400px;
+  }
+  .card{
+    height: 360px;
+    overflow: auto;
+  }
+</style>
 <template>
-  <div style="margin-top:15px">
+  <div style="margin-top:15px;">
     <Button type="primary" @click="ttt">测试重复操作</Button>
+    <Button type="primary" @click="toWebsocketMsg">测试发送消息</Button>
     <Button style="float:right" type="primary" @click="logout">退出</Button>
+    <Row class="websocket">
+      <Col span="11" >
+        <Card class="card" :bordered="false">
+           <Input v-model="sendMessage1" search enter-button="发送" @on-search="search1" />
+           <p style="margin-top: 10px" v-for="(item, index) in message1" :key="index">
+             {{item}}
+           </p>
+        </Card>
+      </Col>
+      <Col span="11" offset="2">
+        <Card class="card" :bordered="false">
+           <Input v-model="sendMessage2" search enter-button="发送" @on-search="search2" />
+           <p style="margin-top: 10px" v-for="(item, index) in message2" :key="index">
+             {{item}}
+           </p>
+        </Card>
+      </Col>
+    </Row>
     <Row>
       <Col>
         <pic></pic>
@@ -14,13 +44,118 @@
 // @ is an alias to /src
 import restApi from '@/api/restApi'
 import pic from '@/components/pic'
+import { webSocketUrl } from '@/utils/globalConfig.js'
 
 export default {
   name: 'Home',
   components: {
     pic
   },
+  data () {
+    return {
+      isConnect: false,
+      websocket: null,
+      webSocket2: null,
+      message1: [],
+      message2: [],
+      sendMessage1: '',
+      sendMessage2: '',
+      sid1: 1,
+      sid2: 2
+    }
+  },
+  watch: {
+  },
   methods: {
+    initWebSoctet () {
+      const vm = this
+      this.websocket = new WebSocket(webSocketUrl + '/websocket/1/1')
+      this.websocket.onopen = function (e) {
+        console.log('WebSocket连接成功')
+        vm.heartCheckStart()
+      }
+      this.websocket.onerror = function (e) {
+        console.log(e)
+        console.log('WebSocket连接失败')
+        vm.reConnect()
+      }
+      this.websocket.onclose = function (e) {
+        console.log(e)
+        console.log('websocket关闭了')
+        vm.reConnect()
+      }
+      this.websocket.onmessage = function (e) {
+        console.log('websocket1接收到消息')
+        // 心跳重置
+        vm.heartReset()
+        console.log(e)
+        var json = JSON.parse(e.data)
+        const message = json.webSocketMessage
+        if (json.type === 'message') {
+          vm.$Notice.success({
+            title: '收到消息',
+            desc: '用户1 收到了来自用户 ' + message.uid + ' 的消息'
+          })
+          vm.message1.unshift(message.sendMsg)
+        }
+      }
+    },
+    initWebSoctet2 () {
+      const vm = this
+      this.webSocket2 = new WebSocket(webSocketUrl + '/websocket/2/1')
+      this.webSocket2.onopen = function (e) {
+        setInterval(() => {
+          vm.webSocket2.send('ping')
+        }, 30000)
+        console.log('WebSocket2连接成功')
+      }
+      this.webSocket2.onerror = function (e) {
+        console.log('WebSocket2连接失败')
+      }
+      this.webSocket2.onclose = function (e) {
+        console.log('websocket2关闭了')
+      }
+      this.webSocket2.onmessage = function (e) {
+        console.log('websocket2接收到消息')
+        console.log(e)
+        var json = JSON.parse(e.data)
+        const message = json.webSocketMessage
+        if (json.type === 'message') {
+          vm.$Notice.success({
+            title: '收到消息',
+            desc: '用户2 收到了来自用户 ' + message.uid + ' 的消息'
+          })
+          vm.message2.unshift(message.sendMsg)
+        }
+      }
+    },
+    // webSocket重连
+    reConnect () {
+      if (this.isConnect) {
+        return
+      }
+      this.isConnect = true
+      // 20秒重连一次
+      setTimeout(() => {
+        this.initWebSoctet()
+        this.isConnect = false
+      }, 20000)
+    },
+    // websocket心跳消息重置
+    heartReset () {
+      clearTimeout(this.timeoutCheck)
+      clearTimeout(this.timeoutClose)
+      this.heartCheckStart()
+    },
+    // websocket心跳发送
+    heartCheckStart () {
+      this.timeoutCheck = setTimeout(() => {
+        this.websocket.send('ping')
+        this.timeoutClose = setTimeout(() => {
+          this.websocket.close()
+        }, 30000)
+      }, 30000)
+    },
     ttt () {
       restApi.ttt().then(data => {
         alert(data)
@@ -29,7 +164,39 @@ export default {
     logout () {
       this.$store.commit('clearUser', null)
       this.$router.push('/')
+    },
+    search1 () {
+      if (this.sendMessage1) {
+        this.websocket.send(JSON.stringify({
+          uid: '1',
+          sid: '2',
+          sendMsg: this.sendMessage1
+        }))
+        this.sendMessage1 = ''
+      }
+    },
+    search2 () {
+      if (this.sendMessage2) {
+        this.webSocket2.send(JSON.stringify({
+          uid: '2',
+          sid: '1',
+          sendMsg: this.sendMessage2
+        }))
+        this.sendMessage2 = ''
+      }
+    },
+    toWebsocketMsg () {
+      this.webSocket2.send(JSON.stringify({
+        uid: '2',
+        sid: '1',
+        sendMsg: '111111111'
+      }))
+      this.sendMessage2 = ''
     }
+  },
+  mounted () {
+    this.initWebSoctet()
+    this.initWebSoctet2()
   }
 }
 </script>
