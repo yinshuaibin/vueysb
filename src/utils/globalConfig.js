b779db935fac8f01a733fc04b0fd4a167bd330d5
yinshuaibin <35283085+yinshuaibin@users.noreply.github.com>
2021-11-28 22:30:51 +0800

    修改配置

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -1,11 +1,11 @@
 // let声明变量只能在当前大括号内使用
 const env = process.env.NODE_ENV
 // const声明的是常亮,默认不可改变
-const domain = 'www.yinshuaibin.xyz'
+const domain = '127.0.0.1:8080'
 // 开发地址
 const devUrl = 'http://127.0.0.1:9998'
 // 生产环境地址
-const prodUrl = 'https://' + domain + '/yinshuaibin'
+const prodUrl = 'http://' + domain + '/yinshuaibin'
 // 开发地址
 const devPicUrl = devUrl + '/image'
 // 生产环境地址
